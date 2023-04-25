d7d5daa2eed491e2dc52d634fe209da898cf378c
Justine-Njuguna <hdenlysjustine@gmail.com>
2023-04-25 01:49:14 -0700

    Betty would be proud

---
+++ betty.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - calculates the sum of two numbers.
+ *
+ * Return: 0 on success.
+ */
+
+int main(void)
+{
+	int num1 = 5, num2 = 5, sum = 0;
+
+	sum = num1 + num2;
+
+	printf("%d is the sum.", sum);
+
+	return (0);
+}
