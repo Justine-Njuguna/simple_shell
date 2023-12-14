e224e1c6937324729ac02b096a09c41cb61eea2a
Justine-Njuguna <hdenlysjustine@gmail.com>
2023-12-14 12:23:42 +0300

    Added Betty

---
+++ betty_file.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("===============\n");
+	printf("Hello World!\n");
+	printf("===============\n");
+
+	return (0);
+}
